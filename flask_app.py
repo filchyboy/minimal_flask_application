a7f39b64abb776e65dfb8fb619b4641954466f96
Your name hereChristopher <christopher.filkins@gmail.com>
2020-03-26 16:57:14 -0700

    Pep8 treatment

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,14 @@
 # flask_app.py - a minimal flsk application
 
-# import flask module 
+# import flask module
 from flask import Flask
 
 # initiate application
 app = Flask(__name__)
 
+
 # define route
 @app.route("/")
-
 # attach a function to that route
 def root():
     return("This is a minimally viable flask application. Git repo <a href='https://github.com/filchyboy/minimal_flask_application'>here</a>.")
